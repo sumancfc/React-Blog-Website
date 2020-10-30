ff8c72034ebcd01da7f4753159b4afb8a1be4a20
sumancfc <sumanstha999@gmail.com>
2020-10-30 13:37:37 +0545

    Breadcrumb component added

---
+++ src/components/common/Breadcrumb/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Breadcrumb.css";
+
+const Breadcrumb = () => {
+  return (
+    <div className='breadcrumb-section'>
+      <div className='container'>
+        <div className='row'>
+          <div className=' col-md-12 col-sm-12 col-xs-12'>
+            <ol className='breadcrumb'>
+              <li className='breadcrumb-item'>
+                <Link to='/'>Home</Link>
+              </li>
+              <li className='breadcrumb-item active'>Contact</li>
+            </ol>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Breadcrumb;
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
+import Breadcrumb from "../common/Breadcrumb";
 import MasonryGrid from "../common/Grid/MasonryGrid";
 import Layout from "../Layout";
 
 const Home = () => {
   return (
     <Layout>
+      <Breadcrumb />
       <MasonryGrid />
     </Layout>
   );
